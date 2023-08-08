59eb3cbc97b38e730a1024e35ff4d096cab0ddf9
Seba Calle <scalle@mulesoft.com>
2023-08-07 22:34:08 -0300

    monday fixes (#3)

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,8 @@
 
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+        //}, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+        }, 800);
 
         // Hide the menu once clicked if mobile
         if ($('header').hasClass('active')) {
